3c33e0b12e740efad9cb69a8799a315227030102
Sylvanus-the-sorcerer <103826382+SylvanusSlytherin@users.noreply.github.com>
2024-01-13 17:32:35 +0330

    next image explained

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
+import styles from "./about.module.css";
 const Aboutpage = () => {
   return (
-    <div>Aboutpage</div>
-  )
-}
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src={"https://images.pexels.com/photos/10917649/pexels-photo-10917649.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} alt="About image" fill />
+      </div>
+    </div>
+  );
+};
 
-export default Aboutpage
+export default Aboutpage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./home.module.css";
+import Image from "next/image";
 const Home = () => {
-  
   return (
-    <div>Home page</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency.</h1>
+        <p>
+          {" "}
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image
+            src={"/brands.png"}
+            alt="Brands Image"
+            fill
+            className={styles.brandImg}
+          />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src={'/hero.gif'} alt="Hero Image" fill className={styles.heroImg}/>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
